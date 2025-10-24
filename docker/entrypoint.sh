80d114532beec4cc3215778a5bf08d0e7134a6da
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2025-10-24 18:38:28 +0100

    Fix: [AEA-0000] - fix sonar issuses (#303)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - fix sonar issues

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,13 +9,10 @@ echo "**************************************"
 echo
 echo
 
-if [ -z "${CDK_APP_PATH}" ]; then
+if [[ -z "${CDK_APP_PATH}" ]]; then
     echo "CDK_APP_PATH is unset or set to the empty string"
     exit 1
 fi
-
-# shellcheck source=/dev/null
-source /home/cdkuser/.asdf/asdf.sh
 
 sed -i -n '/nodejs/p'  /home/cdkuser/workspace/.tool-versions
 cd /home/cdkuser/workspace/ || exit
@@ -23,13 +20,13 @@ cd /home/cdkuser/workspace/ || exit
 asdf install
 asdf reshim nodejs
 
-if [ "${SHOW_DIFF}" = "true" ]
+if [[ "${SHOW_DIFF}" = "true" ]]
 then
     echo "Running diff"
     npx cdk diff \
 		--app "npx ts-node --prefer-ts-exts ${CDK_APP_PATH}"
 fi
-if [ "${DEPLOY_CODE}" = "true" ]
+if [[ "${DEPLOY_CODE}" = "true" ]]
 then
     echo "Running deploy"
     npx cdk deploy \
