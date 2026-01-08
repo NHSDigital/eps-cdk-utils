280324fb912c32d84f91e1f70a211d4b181c0fa1
MatthewPopat-NHS <matthew.popat1@nhs.net>
2026-01-08 14:45:21 +0000

    Added shared code for CDK and Proxygen deployments

--- packages/cdkConstructs/src/index.ts
+++ packages/cdkConstructs/src/index.ts
@@ -1,2 +1,6 @@
 // Export all constructs
 export * from "./constructs/TypescriptLambdaFunction.js"
+export * from "./apps/createApp.js"
+export * from "./config/index.js"
+export * from "./specifications/writeSchemas.js"
+export * from "./specifications/deployApi.js"
