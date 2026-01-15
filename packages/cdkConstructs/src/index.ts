49dff1dde6ab99c5eb88fe4464f49ffc259e1167
MatthewPopat-NHS <matthew.popat1@nhs.net>
2026-01-15 11:46:22 +0000

    Fixed index exports

--- packages/cdkConstructs/src/index.ts
+++ packages/cdkConstructs/src/index.ts
@@ -2,5 +2,3 @@
 export * from "./constructs/TypescriptLambdaFunction.js"
 export * from "./apps/createApp.js"
 export * from "./config/index.js"
-export * from "./specifications/writeSchemas.js"
-export * from "./specifications/deployApi.js"
